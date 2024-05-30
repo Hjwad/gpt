1b05022b0bbb75dcf518d888fe69239b4469200e
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-30 14:26:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -125,10 +125,10 @@ async def must_join_channel(bot: Client, msg: Message):
         except UserNotParticipant:
             if UPDATE_CHNL.isalpha():
                 link = "https://t.me/" + UPDATE_CHNL
-         else:
-                chat_info = await bot.get_chat(UPDATE_CHNL)
-                link = chat_info.invite_link
-            try:
+    else:
+        chat_info = await bot.get_chat(UPDATE_CHNL)
+        link = chat_info.invite_link
+    try:
                 await msg.reply_photo(
                     photo=START_IMG,
                     caption=f"حسب قاعدة البيانات الخاصة بي، لم تنضم بعد إلى [قناة التحديث]({link})، إذا كنت تريد استخدامي، انضم إلى [قناة التحديث]({link}) وابدأ من جديد!",
